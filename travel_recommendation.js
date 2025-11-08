dcb5b8fd31a114d1c68c620cfd61d1300f4872ae
Philippe <philippe.nourry@orange.com>
2025-11-08 09:08:05 -0500

    sauvegarde

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,2 +1,17 @@
 const boutonRecherche=document.getElementById("boutonRecherche");
-const boutonEffacer=document.getElementById("boutonEffacer");
+const boutonEnvoi=document.getElementById("envoi");
+
+boutonEnvoi.addEventListener("click", formulaireEnvoye);
+
+function formulaireEnvoye(){
+    const nom=document.getElementById("nom");
+    const mail=document.getElementById("mail");
+    const message=document.getElementById("message");
+    if (nom && mail && message){
+        messageEnvoye="votre message a bien été envoyé à l'adresse: "+ mail.value
+        alert(messageEnvoye);
+        document.getElementById("nom").value="";
+        document.getElementById("mail").value="";
+        document.getElementById("message").value="";
+    }   
+}
