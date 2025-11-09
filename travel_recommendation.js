5b161f2e5167eec1d4c41b9f09ab99afcedaaf9b
Philippe <philippe.nourry@orange.com>
2025-11-09 04:48:02 -0500

    sauvegarde projet

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,17 +1,29 @@
+const boutonEnvoi=document.getElementById("boutonEnvoi");
 const boutonRecherche=document.getElementById("boutonRecherche");
-const boutonEnvoi=document.getElementById("envoi");
+const boutonEffacer=document.getElementById("boutonEffacer");
 
-boutonEnvoi.addEventListener("click", formulaireEnvoye);
+boutonEnvoi.addEventListener('click', formulaireEnvoye);
+boutonRecherche.addEventListener('click', rechercheDestination);
+BoutonEffacer.addEventListener('click', effacerRecherche);
 
 function formulaireEnvoye(){
-    const nom=document.getElementById("nom");
-    const mail=document.getElementById("mail");
-    const message=document.getElementById("message");
+    const nom=document.getElementById("nom").value;
+    const mail=document.getElementById("mail").value;
+    const message=document.getElementById("message").value;
     if (nom && mail && message){
-        messageEnvoye="votre message a bien été envoyé à l'adresse: "+ mail.value
+        messageEnvoye="votre message a bien été envoyé à l'adresse: "+ mail
         alert(messageEnvoye);
         document.getElementById("nom").value="";
         document.getElementById("mail").value="";
         document.getElementById("message").value="";
     }   
+}
+
+function rechercheDestination(){
+    const Recherche=document.getElementById("recherche").value.toLowercase();
+    console.log(recherche);
+}
+
+function effacerRecherche(){
+    document.getElementById("recherche").value="";
 }
