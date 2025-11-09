f6b4cb72015ba82756699c8179c1fd6683695a3e
Philippe <philippe.nourry@orange.com>
2025-11-09 06:38:32 -0500

    sauvegarde projet

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,27 +1,31 @@
 const boutonRecherche = document.getElementById("boutonRecherche");
 const boutonEffacer = document.getElementById("boutonEffacer");
-
+const tableCle=[
+    {cle: "beaches", valeur: "beach"},
+    {cle: "temples", valeur: "temple"},
+    {cle: "countries", valeur: "countrie"}];
 
 boutonRecherche.addEventListener("click", rechercheDestination);
 boutonEffacer.addEventListener("click", effacerRecherche);
 
-function formulaireEnvoye(){
-    const nom=document.getElementById("nom").value;
-    const mail=document.getElementById("mail").value;
-    const message=document.getElementById("message").value;
-    if (nom && mail && message){
-        messageEnvoye="votre message a bien été envoyé à l'adresse: "+ mail
-        alert(messageEnvoye);
-        document.getElementById("nom").value="";
-        document.getElementById("mail").value="";
-        document.getElementById("message").value="";
-    }   
-}
+
 
 function rechercheDestination(){
     const recherche=document.getElementById("recherche").value.toLowerCase();
-    console.log(recherche);
-}
+    for (index=0; index<tableCle.length; index++){
+        if (recherche==tableCle[index].cle) {
+            rechercheFinale=tableCle[index].valeur;
+        }
+        else {
+            rechercheFinale=recherche;
+        }
+    }
+console.log(recherches);
+    fetch('./travel_recommendation_api.json')
+        .then (response => response.json());
+        //.then(data =>{
+        
+        }
 
 function effacerRecherche(){
     document.getElementById("recherche").value="";
