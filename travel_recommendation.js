509750c58475b66f6052d86d90d81b3c95a08b21
Philippe <philippe.nourry@orange.com>
2025-11-09 09:24:12 -0500

    sauvegarde projet

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,9 +1,9 @@
 const boutonRecherche = document.getElementById("boutonRecherche");
 const boutonEffacer = document.getElementById("boutonEffacer");
 const tableCle=[
-    {cle: "beaches", valeur: "beach"},
-    {cle: "temples", valeur: "temple"},
-    {cle: "countries", valeur: "countrie"}];
+    {cle: "beach", valeur: "beaches"},
+    {cle: "temple", valeur: "temples"},
+    {cle: "countrie", valeur: "countries"}];
 
 boutonRecherche.addEventListener("click", rechercheDestination);
 boutonEffacer.addEventListener("click", effacerRecherche);
@@ -11,21 +11,30 @@ boutonEffacer.addEventListener("click", effacerRecherche);
 
 
 function rechercheDestination(){
+    //const resultdiv=document.getElementById("result");
     const recherche=document.getElementById("recherche").value.toLowerCase();
     for (index=0; index<tableCle.length; index++){
         if (recherche==tableCle[index].cle) {
             rechercheFinale=tableCle[index].valeur;
+            break;
         }
         else {
             rechercheFinale=recherche;
         }
     }
-console.log(recherches);
-    fetch('./travel_recommendation_api.json')
-        .then (response => response.json());
-        //.then(data =>{
-        
-        }
+
+}
+fetch('travel_recommendation_api.json')
+    .then (response => {return response.json();})
+    //.then (data => {console.log(data);})
+    .then (data => {
+        const condition = data.${rechercheFinale}.find(item => item.name.toLowerCase()==="australia");
+        if(condition){
+            const pays = condition.name;
+            console.log(pays);
+                }
+    })
+
 
 function effacerRecherche(){
     document.getElementById("recherche").value="";
