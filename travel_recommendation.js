eab40e89e3b8ff10bfec48b41fc5f8160b2ef8f5
Philippe <philippe.nourry@orange.com>
2025-11-10 05:40:43 -0500

    sauvegarde projet

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -15,6 +15,7 @@ function rechercheDestination(){
     //const resultdiv=document.getElementById("result");
     const recherche=document.getElementById("recherche").value.toLowerCase();
     const affichage=document.getElementById("affichageResultat");
+    affichage.innerHTML =``;
     for (index=0; index<tableCle.length; index++){
         if (recherche==tableCle[index].cle) {
             rechercheFinale=tableCle[index].valeur;
@@ -27,16 +28,19 @@ function rechercheDestination(){
 fetch('travel_recommendation_api.json')
     .then (response => {return response.json();})
     .then (data => {
-        //console.log(data);
-        const resultat=data[rechercheFinale];//.find((element) => element.name==="Australia");
+        const resultat=data[rechercheFinale];
         console.log(resultat);
-        console.log(resultat[0].name);
-        affichage.innerHTML +=`<h2>${resultat[0].name}<h2>`;
-        affichage.innerHTLM +=`<img scr="${resultat[0].imageUrl}" alt="hjh">`;
-        affichage.innerHTML +=`<p>${resultat[0].description}<p>`;
+        for (index=0;index<resultat.length; index++)
+        {
+            affichage.innerHTML +=`<h2>${resultat[index].name}<h2>`;
+            affichage.innerHTML +=`<img scr="${resultat[index].imageUrl}" alt="angkor">`;
+            affichage.innerHTML +=`<p>${resultat[index].description}<p>`;
+        }
         });
     
 }
 function effacerRecherche(){
+    const affichage=document.getElementById("affichageResultat");
     document.getElementById("recherche").value="";
+    affichage.innerHTML =``;
 }
