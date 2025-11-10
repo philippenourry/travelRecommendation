ee5a854cc34929edd4ceded9e17db7c30d086dfd
Philippe <philippe.nourry@orange.com>
2025-11-10 09:07:37 -0500

    sauvegarde projet

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,5 +1,7 @@
 const boutonRecherche = document.getElementById("boutonRecherche");
 const boutonEffacer = document.getElementById("boutonEffacer");
+const affichage=document.getElementById("affichageResultat");
+affichage.style.display="none";
 
 const tableCle=[
     {cle: "beach", valeur: "beaches"},
@@ -15,6 +17,7 @@ function rechercheDestination(){
     //const resultdiv=document.getElementById("result");
     const recherche=document.getElementById("recherche").value.toLowerCase();
     const affichage=document.getElementById("affichageResultat");
+    affichage.style.display="block";
     affichage.innerHTML =``;
     for (index=0; index<tableCle.length; index++){
         if (recherche==tableCle[index].cle) {
@@ -32,20 +35,22 @@ fetch('travel_recommendation_api.json')
         if (rechercheFinale==="countries"){
             for (let i = 0; i < resultat.length; i++){
                 console.log(resultat[i]);
-                for(let j = 0; j < resultat[i].length; j++){
-                    affichage.innerHTML +=`${i}`;
-                    //affichage.innerHTML +=`<h2>${resultat[j][i].name}<h2>`;
-                    //affichage.innerHTML +=`<img src="${resultat[j][i].imageUrl}" alt="angkor">`;
-                    //affichage.innerHTML +=`<p>${resultat[j][i].description}<p>`;
+                console.log((resultat[i].cities).length);
+                for(let j = 0; j < resultat[i].cities.length; j++){
+                    affichage.innerHTML +=`<img src="${resultat[i].cities[j].imageUrl}">`;
+                    affichage.innerHTML +=`<h3>${resultat[i].cities[j].name}<h3>`;
+                    affichage.innerHTML +=`<p>${resultat[i].cities[j].description}<p>`;
+                    affichage.innerHTML +=`<p><button>Reservation</button></p>`;
                 }
             }
         }
         else{
             for (index=0;index<resultat.length; index++)
             {
-                affichage.innerHTML +=`<h2>${resultat[index].name}<h2>`;
-                affichage.innerHTML +=`<img src="${resultat[index].imageUrl}" alt="angkor">`;
-                affichage.innerHTML +=`<p>${resultat[index].description}<p>`;
+                affichage.innerHTML +=`<img src="${resultat[index].imageUrl}">`;
+                affichage.innerHTML +=`<h3>${resultat[index].name}<h3>`;
+                affichage.innerHTML +=`<p>${resultat[index].description}</p>`;
+                affichage.innerHTML +=`<p><button>Recherche</button></p>`;
             }
         }
         });
@@ -55,4 +60,5 @@ function effacerRecherche(){
     const affichage=document.getElementById("affichageResultat");
     document.getElementById("recherche").value="";
     affichage.innerHTML =``;
+    affichage.style.display="none";
 }
