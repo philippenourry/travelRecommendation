5123cc2b8b4d6ce9f929b3982ca1a3564a53b4b3
Philippe <philippe.nourry@orange.com>
2025-11-11 04:42:45 -0500

    sauvegarde projet

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,12 +1,18 @@
 const boutonRecherche = document.getElementById("boutonRecherche");
 const boutonEffacer = document.getElementById("boutonEffacer");
 const affichage=document.getElementById("affichageResultat");
+let resultat=[];
+let id=[];
 affichage.style.display="none";
 
 const tableCle=[
     {cle: "beach", valeur: "beaches"},
+    {cle: "beache", valeur: "beaches"},
     {cle: "temple", valeur: "temples"},
-    {cle: "countrie", valeur: "countries"}];
+    {cle: "countrie", valeur: "countries"},
+    {cle: "countire", valeur: "countries"},
+    {cle: "countires", valeur: "countries"},
+];
 
 boutonRecherche.addEventListener("click", rechercheDestination);
 boutonEffacer.addEventListener("click", effacerRecherche);
@@ -31,16 +37,14 @@ function rechercheDestination(){
 fetch('travel_recommendation_api.json')
     .then (response => {return response.json();})
     .then (data => {
-        const resultat=data[rechercheFinale];
+        resultat=data[rechercheFinale];
         if (rechercheFinale==="countries"){
             for (let i = 0; i < resultat.length; i++){
-                console.log(resultat[i]);
-                console.log((resultat[i].cities).length);
                 for(let j = 0; j < resultat[i].cities.length; j++){
                     affichage.innerHTML +=`<img src="${resultat[i].cities[j].imageUrl}">`;
                     affichage.innerHTML +=`<h3>${resultat[i].cities[j].name}<h3>`;
                     affichage.innerHTML +=`<p>${resultat[i].cities[j].description}<p>`;
-                    affichage.innerHTML +=`<p><button>Reservation</button></p>`;
+                    affichage.innerHTML +=`<p><button id=[${i},${j}] onclick=boutonReserver(id)>Reservation</button></p>`;
                 }
             }
         }
@@ -50,7 +54,7 @@ fetch('travel_recommendation_api.json')
                 affichage.innerHTML +=`<img src="${resultat[index].imageUrl}">`;
                 affichage.innerHTML +=`<h3>${resultat[index].name}<h3>`;
                 affichage.innerHTML +=`<p>${resultat[index].description}</p>`;
-                affichage.innerHTML +=`<p><button>Recherche</button></p>`;
+                affichage.innerHTML +=`<p><button id=${index} onclick=boutonReserver(id)>Reservation</button></p>`;
             }
         }
         });
@@ -61,4 +65,13 @@ function effacerRecherche(){
     document.getElementById("recherche").value="";
     affichage.innerHTML =``;
     affichage.style.display="none";
+}
+function boutonReserver(id){
+    if (id.length>1){
+        message=("votre réservation a bien été prise en compte pour " + resultat[id[1]].cities[id[3]].name);
+    }
+    else{
+        message=("votre réservation a bien été prise en compte pour " + resultat[id].name);
+    }
+    alert (message);
 }
